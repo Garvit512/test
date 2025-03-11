5c538df1fe23a97d436335cb1842938b91a6a35d
Garvit Kumar Arya <36696796+Garvit512@users.noreply.github.com>
2025-03-11 18:35:27 +0530

    Update job.py

--- job.py
+++ job.py
@@ -1,1 +1,13 @@
-print("hello")
+import json
+
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        config = json.load(file)
+    return config
+  
+if __name__ == "__main__":
+    config_file_path = 'config.json'  # Adjust the path if necessary
+    config = load_config(config_file_path)
+    print(json.dumps(config, indent=4))  # Pretty print the JSON content
+    print("hello")
+
